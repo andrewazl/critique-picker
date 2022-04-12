29059537280e02d06e3bdd496ae3953efcb1b80d
andrewl <andrewl@bu.edu>
2022-04-11 20:09:53 -0400

    Added initial script

---
+++ critiques.py
@@ -0,0 +1,18 @@
+import random
+
+students = ["MA","TB","CB","MB","KC","HC","AC","JD","BF","BG","CH","BK","GL","DM","RN","NP","FS","KS","AW","BZ"]
+pool = students*3
+
+print(students)
+print(pool)
+
+for student in students:
+    critiques = set()
+    for i in range(0, 3):
+        critique = random.choice(pool)
+        while(critique == student or critique in critiques):
+            critique = random.choice(pool)
+        critiques.add(critique)
+        pool.remove(critique)
+    print(student + ': ' + "{}".format(critiques))
+    #print(critiques)
